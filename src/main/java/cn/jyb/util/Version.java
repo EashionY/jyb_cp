20ebcf4132966e2de4f7ecd33755a6a59392408c
EashionY <silver_kira@live.com>
2017-07-17 17:56:02 +0800

    添加静态变量UPDATEURL(版本更新地址)

--- src/main/java/cn/jyb/util/Version.java
+++ src/main/java/cn/jyb/util/Version.java
@@ -1,11 +1,17 @@
 package cn.jyb.util;
 
 public class Version {
-
+	//版本号
 	private static final String VERSION = "1.0.1";
+	//软件更新下载地址
+	private static final String UPDATEURL = "http://shouji.baidu.com/software/11840081.html";
 
 	public static String getVersion() {
 		return VERSION;
+	}
+
+	public static String getUpdateurl() {
+		return UPDATEURL;
 	}
 	
 }
